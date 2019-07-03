28164190e64cabd11e2a1ed73d0c8f48b3323db4
Vodkamishka <andrtomsk@yandex.com>
2019-07-03 14:36:06 +0700

    Updates

---
+++ precache-manifest.de4ff083cbd66cb2f0363042e5fbcf0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6fe3e7ea7fa14231d1396cf932b360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e0ed33af124fe14157",
+    "url": "/static/css/main.a93cb38f.chunk.css"
+  },
+  {
+    "revision": "56403a8ede5f883af9b1",
+    "url": "/static/js/2.f0aab62d.chunk.js"
+  },
+  {
+    "revision": "f4e0ed33af124fe14157",
+    "url": "/static/js/main.6dfc30e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
